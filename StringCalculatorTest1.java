0cff910270f5d72ff957913ccaede9bfe1b481b0
NIHAR RANJAN PALLEYI <68610917+niharpalleyi@users.noreply.github.com>
2021-01-02 22:37:33 +0530

    Update StringCalculatorTest1.java

--- StringCalculatorTest1.java
+++ StringCalculatorTest1.java
@@ -1,7 +1,7 @@
 package stringcalculator;
 import java.util.*;
 
-public class StringCalculator {
+public class StringCalculatorTest1 {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
